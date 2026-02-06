29a0bbdd885b4af2d396293fdf8e73fd98b1b4ed
Leonardo Heis <leonardo.heis@gmail.com>
2026-02-06 19:10:44 -0300

    fix(config): change server host from 0.0.0.0 to 127.0.0.1 for improved local development

--- backend/src/upskills/db/sqlite.py
+++ backend/src/upskills/db/sqlite.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.asyncio import (
     create_async_engine,
 )
 
-from upskills.repositories.base import Base
+from upskills.repositories.base.models import Base
 
 from .provider import DatabaseProvider
 
